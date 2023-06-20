8973816edbe95c7447bb19b9ee11d9ccaca69349
yutaroadachi <48372923+yutaroadachi@users.noreply.github.com>
2023-06-20 20:57:47 +0900

    feat: introduce panda css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import { css } from "../../styled-system/css";
+
 export default function Page() {
   return (
     <main>
-      <h1>Hello World!</h1>
+      <h1 className={css({ fontSize: "2xl", fontWeight: "bold" })}>
+        Hello 🐼!
+      </h1>
     </main>
   );
 }
